f67a93ee8cb5c4d516ad42f3a141213b75a76851
GAKIZA <imansamgis@gmail.com>
2023-05-26 17:55:36 -0700

    group project

---
+++ tokenizer.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/**
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+char **strtow(char *str, char *d)
+{
+	int a, b, c, e, numwords = 0;
+	char **z;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (a = 0; str[a] != '\0'; a++)
+		if (!is_delim(str[a], d) && (is_delim(str[a + 1], d) ||
+					!str[a + 1]))
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	z = malloc((1 + numwords) * sizeof(char *));
+	if (!z)
+		return (NULL);
+	for (a = 0, b = 0; b < numwords; b++)
+	{
+		while (is_delim(str[a], d))
+			a++;
+		c = 0;
+		while (!is_delim(str[a + c], d) && str[a + c])
+			c++;
+		z[b] = malloc((c + 1) * sizeof(char));
+		if (!z[b])
+		{
+			for (c = 0; c < b; c++)
+				free(z[c]);
+			free(z);
+			return (NULL);
+		}
+		for (e = 0; e < c; e++)
+			z[b][e] = str[a++];
+		z[b][e] = 0;
+	}
+	z[b] = NULL;
+	return (z);
+}
+/**
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+char **strtow2(char *str, char d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+				(str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+			for (m = 0; m < k; m++)
+				s[j][m] = str[i++];
+			s[j][m] = 0;
+	}
+		s[j] = NULL;
+		return (s);
+}
